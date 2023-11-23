7445677ac823c36342be8d2631c6b255097e89e6
TiltMeSideways <151081489+TiltMeSideways@users.noreply.github.com>
2023-11-23 22:46:46 +0000

    Add files via upload

---
+++ practice101/script.js
@@ -0,0 +1,32 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if (inputBox.value === ''){
+        alert("Please enter a value.");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+
+}
+listContainer.addEventListener("click", function(e){
+    e.target.parentElement.remove("SPAN");
+    saveData();
+    }, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask(); 
